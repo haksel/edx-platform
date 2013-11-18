29827ed666b93571e8f6c9c1d9ba26cdedd37ab1
Ned Batchelder <ned@nedbatchelder.com>
2013-11-18 14:23:04 -0500

    WIP

--- common/lib/xmodule/xmodule/x_module.py
+++ common/lib/xmodule/xmodule/x_module.py
@@ -8,6 +8,7 @@ from lxml import etree
 from collections import namedtuple
 from pkg_resources import resource_listdir, resource_string, resource_isdir
 from webob import Response
+from webob.multidict import MultiDict
 
 from xmodule.modulestore import Location
 from xmodule.modulestore.exceptions import ItemNotFoundError, InsufficientSpecificationError, InvalidLocationError
@@ -405,7 +406,14 @@ class XModule(XModuleMixin, HTMLSnippet, XBlock):  # pylint: disable=abstract-me
         """
         XBlock handler that wraps `handle_ajax`
         """
-        response_data = self.handle_ajax(suffix, request.POST)
+        # WebOb requests have multiple entries for uploaded files.  handle_ajax
+        # expects a single entry as a list.
+        request_post = MultiDict(request.POST)
+        for key in set(request.POST.iterkeys()):
+            if hasattr(request.POST[key], "file"):
+                request_post[key] = request.POST.getall(key)
+
+        response_data = self.handle_ajax(suffix, request_post)
         return Response(response_data, content_type='application/json')
 
     def get_children(self):
